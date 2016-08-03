570dd5fe7ce22e134275e56f1d540da0e1cc4708
trentmarino <trentmario@hotmail.com>
2016-08-03 19:34:44 +1000

    changed the gitignore to not allow the dev connect file to be added

--- web/db_connect.php
+++ web/db_connect.php
@@ -6,6 +6,10 @@ $username = "forge";
 $password = "JAAvMz4RhIyLAocRwTUP";
 $db = "forge";
 
+?>
+<script>console.log('big juanz');</script>
+<?php
+
 $conn = new mysqli($server, $username, $password, $db);
 
 
