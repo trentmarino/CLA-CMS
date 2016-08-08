c7f789950fcb681c11a47bd1737b284c2a3a3292
trentmarino <trentmario@hotmail.com>
2016-08-08 22:07:25 +1000

    added customer insert script needs to be prepared though

---
+++ web/postCustomer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trentmarino
+ * Date: 8/08/2016
+ * Time: 7:12 PM
+ */
+
+echo "yep";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    require "db_connect_production.php";
+    echo "yep";
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $location = $_POST['location'];
+        $checkin = $_POST['checkin'];
+        $checkout = $_POST['checkout'];
+        $roomtype = $_POST['roomtype'];
+        $numberOfGuests = $_POST['numberofguests'];
+        $pricePaid = $_POST['pricepaid'];
+        $creditcardNumber = $_POST['creditcardNumber'];
+        $creditcardExpiry = $_POST['creditcardExpiry'];
+        $creditcardCode = $_POST['creditcardCode'];
+
+        $sql = "INSERT INTO customers(name, email,phone,address,
+        location,checkin,checkout,roomtype,numberOfGuests,pricePaid,
+        creditcardNumber,creditcardExpiry,creditcardCode)
+        VALUES ('$name','$email','$phone', '$address','$location','$checkin','$checkout',
+        '$roomtype','$numberOfGuests','$pricePaid','$creditcardNumber','$creditcardExpiry',$creditcardCode) ";
+        echo $sql;
+        if ($conn->query($sql) === TRUE) {
+            echo $sql;
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
+
+        $conn->close();
+
+}
