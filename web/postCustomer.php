6543ff960f0a5107c6a2684afabaa22037bc6d35
trentmarino <trentmario@hotmail.com>
2016-08-15 16:21:31 +1000

    added the stripe api with a test payment charge script

--- web/postCustomer.php
+++ web/postCustomer.php
@@ -8,40 +8,37 @@
 
 echo "yep";
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require "db_connect_production.php";
     echo "yep";
 
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $address = $_POST['address'];
-        $location = $_POST['location'];
-        $checkin = $_POST['checkin'];
-        $checkout = $_POST['checkout'];
-        $roomtype = $_POST['roomtype'];
-        $numberOfGuests = $_POST['numberofguests'];
-        $pricePaid = $_POST['pricepaid'];
-        $creditcardNumber = $_POST['creditcardNumber'];
-        $creditcardExpiry = $_POST['creditcardExpiry'];
-        $creditcardCode = $_POST['creditcardCode'];
-        $lateCheckin = $_POST['lateCheckIn'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $location = $_POST['location'];
+    $checkin = $_POST['checkin'];
+    $checkout = $_POST['checkout'];
+    $roomtype = $_POST['roomtype'];
+    $numberOfGuests = $_POST['numberofguests'];
+    $pricePaid = $_POST['pricepaid'];
+    $creditcardNumber = $_POST['creditcardNumber'];
+    $lateCheckin = $_POST['lateCheckIn'];
 
-        $sql = "INSERT INTO customers(name, email,phone,address,
+
+    $sql = "INSERT INTO customers(name, email,phone,address,
         location,checkin,checkout,roomtype,numberOfGuests,pricePaid,
-        creditcardNumber,creditcardExpiry,creditcardCode, lateCheckIn)
+        creditcardNumber, lateCheckIn)
         VALUES ('$name','$email','$phone', '$address','$location','$checkin','$checkout',
-        '$roomtype','$numberOfGuests','$pricePaid','$creditcardNumber','$creditcardExpiry',$creditcardCode, $lateCheckin) ";
+        '$roomtype','$numberOfGuests','$pricePaid','$creditcardNumber', $lateCheckin) ";
+    echo $sql;
+    if ($conn->query($sql) === TRUE) {
         echo $sql;
-        if ($conn->query($sql) === TRUE) {
-            echo $sql;
-            echo "Record updated successfully";
-        } else {
-            echo "Error updating record: " . $conn->error;
-        }
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
 
-        $conn->close();
+    $conn->close();
 
 }
-
-
